59c23e53f03512514b2950d5e23f59c52efaf295
a-shirshov <artyomsh01@yandex.ru>
2023-09-26 13:07:17 +0300

    new main in c

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    printf("Hello world!\n");
+    char name[20];
+    printf("Enter name: ");
+    scanf("%s", name);
+    printf("Your name is %s.\n", name);
     return 0;
 }
